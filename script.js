6a15f88564cc0d2f350f02f23205bba738c1ed29
webdesign4me-code <jalparri222@gmail.com>
2026-04-02 22:06:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const scrollBar = document.getElementById('scrollBar');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  scrollBar.style.width = scrollPercent + '%';
+});
